904d974b2d1ea6564983aab64ac3caa4e9d93709
wzp1216 <wzp1216@163.com>
2022-01-22 23:08:38 +0800

    add draw-contour1.py

---
+++ opencv/draw_contour1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+
+img=cv.imread("test.jpg")
+imgray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+ret,thresh=cv.threshold(imgray,170,255,0)
+contours,hierarchy=cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+
+out=cv.drawContours(img,contours,-1,(0,255,0),3)
+cv.imshow("out",out)
+cv.waitKey()
+cv.destroyAllWindows()
+
+
