99b84b08169dfa54938a9b2243921b47bb08d5b8
wzp1216 <wzp1216@163.com>
2022-02-11 22:04:46 +0800

    add printdir.c

---
+++ gccccp/printdir.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+void printdir(const char *dir, int depth)
+{
+    DIR *dp;
+    struct dirent *entry;
+    struct stat statbuf;
+    dp=opendir(dir);
+
+    if (dp==NULL) {
+        fprintf(stderr,"not open directory: %s\n",dir);
+        return;
+    }
+    chdir(dir);
+    while((entry=readdir(dp))!=NULL) {
+        lstat (entry->d_name,&statbuf);
+        if (S_ISDIR (statbuf.st_mode)) {
+            if (strcmp(".",entry->d_name)  ==0 ||
+                    strcmp("..",entry->d_name) ==0 )
+                continue;
+            printf("%*s%s /\n",depth, " ",entry->d_name);
+            printdir(entry->d_name,depth+4);
+        }
+        else 
+            printf("%*s%s\n",depth," ",entry->d_name);
+    }
+    chdir("..");
+    closedir(dp);
+}
+
+int main()
+{
+    printf("Directory san of /home:\n*");
+    printdir("/home/wzy/wzp/mysavecode",0);
+    printf("Done.\n");
+    exit(0);
+}
+
+    
