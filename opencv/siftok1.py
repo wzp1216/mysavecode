3aea531fd1ef54b8f7a243fc129d90b182f34b11
wzp1216 <wzp1216@gmail.com>
2022-12-01 10:42:47 +0800

    change sift

--- opencv/siftok1.py
+++ opencv/siftok1.py
@@ -1,10 +1,16 @@
 import numpy as np
 import cv2 as cv
+from matplotlib import pyplot as plt
+
 img=cv.imread('./image/home.jpg')
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-<<<<<<< HEAD
+
 sift=cv.SIFT_create()
-=======
-sift=cv.xfeatures2d.SIFT_create()
->>>>>>> b24a16190fc6175294f270c1f425eeddb4b7f453
+
+kp=sift.detect(gray,None)
+img=cv.drawKeypoints(gray,kp,img)
+
+plt.imshow(img)
+plt.show()
+
 
