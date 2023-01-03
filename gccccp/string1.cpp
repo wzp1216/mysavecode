cc67f4363afabe773c76247203193ba042215eed
wzp1216 <wzp1216@163.com>
2023-01-03 21:22:51 +0800

    add c++primerplus

--- gccccp/string1.cpp
+++ gccccp/string1.cpp
@@ -1,36 +1,29 @@
-#include <iostream>
-#include <sstream>
 #include <string>
-#include <cstring>
-using namespace std;
-struct ROAD{
-    int A;
-    int B;
-    int t;
-};
+#include <iostream>
+#include <cstdio>
+#include <sstream>
+int main()
+{
+    // greet the user
+    std::string name,line;
+    std::cout << "What is your name? ";
+    std::getline(std::cin, name);
+    std::cout << "Hello " << name << ", nice to meet you.\n";
+    // read file line by line
+    int sum = 0;
+    std::getline(std::cin,line);
+    while(1){
+    try{
+         std::stoi(line);
+    }
+    catch (...) {
+        break;
+    }
+        sum += std::stoi(line);
+        std::getline(std::cin,line);
+    }
 
-int main(){
-    //村庄数N   公路数M  公路双向 修好路所用时间t 
-    //read from file title.in
-    //4 4
-    //1 2 6
-    //1 4 4 
-    //1 4 5
-    //4 2 3
-    freopen("title.in","r",stdin);
-    int village,road;
-    cin>>village>>road;
-    int V[village];
-    ROAD R[road];
-    for(int i=0;i<road;i++)
-        cin>>R[i].A>>R[i].B>>R[i].t;
-
-
-    for(int i=0;i<road;i++)
-        cout<<R[i].A<<R[i].B<<R[i].t<<endl;
-
-
-    return 0;
+    std::cout << "\nThe sum is " << sum << ".\n\n";
+   return 0;
 }
-
-
+ 
