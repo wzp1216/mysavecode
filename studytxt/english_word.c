4ae257eda17d0b9779dedd2df31dd89173fa43e4
wzp1216 <wzp1216@163.com>
2023-01-14 21:53:56 +0800

    20230114

--- studytxt/english_word.c
+++ studytxt/english_word.c
@@ -4,7 +4,24 @@ agri
 ann
 astro
 audi
+vint-vict
+form
+prehend
+od
+bar-bal-ban
+vi-via
+script-scrib
+techn-techno
+press
+rect
+flu
+st-sist-stat-stin-stem
 
+
+前缀prefix
+
+
+后缀suffix
 
 
 
@@ -33,130 +50,4 @@ astro, aster=star 星
 audi(){
 audi, audit=hear 听                          audible 听得见的
 }
-
-bell(){
-bell=war 战争                                    rebellion 反叛，反抗
-}
-
-bio(){
-bio, bi=life 生命，生物                      biology 生物学
-}
-
-brev=short 短                                   abbreviate 缩短，节略
-ced, ceed=go 行走                           precedent 先行的，在前
-cept=take 拿，取                              exception 例外，除外
-circ=ring 环，,圆                               circle 圆，圈，环状物
-cid, cis=cut, kill 切，杀                      suicide 自杀
-claim, clam=cry, shout 喊叫              exclaim 呼喊，惊叫
-clar=clear 清楚的，明白的                clarify 澄清；使清楚
-clud, clus=close, shut 关闭               exclude 排斥，拒绝，接纳
-cogn=know 知道                               cognition 认知
-cord=heart 心                                   cordial 衷心的，诚心的
-corpor, cor=body 体                         corporation 团体，社团
-cred=believe, trust 相信，信任         credibility 可信，可靠，
-cruc=cross 十字                               crucify把……钉在十字桇上；折磨
-cur=care 关心，挂念，注意              security 安全
-cru, curs, cour, cours=run 跑           cruise巡航舰
-di=day 日                                         diary 日记
-dit=give 给                                       edit 编辑
-dict,dic=say 言，说                          dictator 独裁者，口授者
-duc, duct=lead 引导                         conduct 引导，指导，经营
-ed=eat 吃                                         edible 可以吃的，食用的
-ev=age 年龄，时代                          longevity 长寿，长命
-fact, fac=do, make 做，作                factory 工厂
-fer=bring, carry 带，拿                    different 不同的，相异的
-flor, olour=flower 花                         florid 如花的，华丽的
-flu=flow 流                                        fluency 流利，流畅
-fus=pour 灌，流，倾泻                    refuse 拒绝，拒受
-geo=earth 地                                   geography 地理学
-gon=angle角                                    trigon 三角形
-grad=step, go, grade 步，走，级    gradual 逐步的
-gram=write 写                                  telegram 电报
-graph=write, writing 写，画              photograph 照相，拍照
-gress=go, walk 行走                        progress 进步
-hap=chance,luck,accident 机会，偶发           happen 发生，巧遇
-hibit=hold 拿，持                             exhibit 展出，展览
-hospit, hosp=guest 客人                  hospitable 好客的
-idio=particular, own, private, proper 特殊的，个人的，专有的          idiom 惯用语，方言
-insul=island 岛                                 insular 岛的，偏狭的
-it=go 行走                                        exit 出口，退出
-ject=throw 投掷                                projection 投掷，发射
-lect, leg, lig=choose, gather 选，收          elect 选举
-lev=raise 举、升                              elevate 抬起，使升高
-liber=liberty 自由                              liberation 解放
-lingu=language 语言                        linguist 语言专家
-liter=letter 文字，字母                      literate 识字的，有文化的
-loc=place 地方                                 local 当地的
-log=speak 言，说                            dialogue 对话
-loqu=speak 言说                              eloquent 有口才的，雄辩的
-lun=moon 月亮                                 lunar 月亮的，似月的
-nanu, man=hand 手                         manuscript 手稿
-mar=sea 海                                      marine 海上的，航海的
-medi=middle 中间                             mediate 居中调解，调停
-memor=memory,mindful 记忆，记住的            memory 记忆，记忆力
-milit=soldier 兵                                  military 军事的，军队的
-mini=small,less 小                             minimum 最小数
-mir=wonder 惊奇，惊异                    admire 赞赏，钦佩
-mort=death 死                                  mortal 终有一死的
-mot=move 移动                                motion 运动，动
-nomin=name 名                                nominal 名义上的，有名无实的
-
-nov=new 新                                      novel 新的，新奇的
-numer=number 数                            numeral 数字，[语]数词
-oper=work 工作                                operation 手术，工作，操作
-ori=rise 升起                                     orient 东方，东方的
-paci=peace 和平                               pacify 使和平，抚慰
-past=feed 喂，食                              pasture 放牧、牧场，吃草
-pel=push, drive 推，逐，驱              propel 推动
-pend, pens=hang 悬挂                     pendent 悬空的，悬而未决的
-pet=seek 追求                                  compete 竞争，比赛
-phon=sound 声音                             phone 电话
-plen=full 满，全                                plenty 大量，丰富
-pone=put 放置                                  postpone 推后，推迟
-popul=people 人民                           population 人口，全体居民
-port=carry 拿，带，运                      import 输入，进口
-pos=put 放置                                    expose 揭露，揭发
-preci=price 价值                                precious 宝贵的，珍贵的
-pur=pure 清，纯                               purify 使纯净
-rect=right, straight 正，直                 correct 改正，纠正
-rupt=break 破                                   rupture 破裂，使裂开
-sal=salt 盐                                        salary 薪水
-sci=know 知                                      science 科学
-sec, sequ=follow 跟随                       sequence 继续，连续
-sect=cut 切割                                    section 切开，一部分
-sent, sens=feel 感觉                         sentiment 感情，思想感情
-son=sound 声音                                sonic 声音的，音速的
-spect=look 看                                    spectate 出席，观看
-spir=breathe 呼吸                             inspire 鼓舞，吸入
-tail=cut 切，割                                  tailor 裁缝，成衣商
-tain, ten=hold 握，持，守                 contain 容纳，包含，内装
-tect=cover 掩盖                                detect 侦查，发觉
-tempor=time 时                                 temporary 暂时的，临时的
-tend, tens=draw 拉                           tension 拉紧，引力
-tent, tract=draw 拉，抽，引              tractor 拖拉机
-urb=city 城市                                    suburb 郊区，近郊
-ut=use 用                                         utility 效用, 有用
-vac, vacu=empty 空                         vacancy 空白，空虚
-vad, vas=walk, go 行走                    invasion 入侵，侵略
-vari=change 变化                            variable 可变的，反复的
-ven=come 来                                   convene 召集（会议），集会
-vert, vers=turn 转                            subvert 推翻，颠覆
-vi, via=way 路                                  via 取道，经由
-vis, vid=see 看                                 visible 可见的，看得见的
-vit=life 生命                                      vital 充满活力的
-viv=live 活                                        vivid 活泼的，有生气的
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
