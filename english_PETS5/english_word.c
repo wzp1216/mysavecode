87003d2ac399b195cbfe6716e0cfe1a1ce526ae5
wzp1216 <wzp1216@163.com>
2023-01-16 22:53:18 +0800

    20230115

---
+++ english_PETS5/english_word.c
@@ -0,0 +1,73 @@
+词根                                                  例词
+ag
+agri
+ann
+astro
+audi
+vint-vict
+
+前缀prefix
+
+
+后缀suffix
+
+
+
+
+ag(){
+ag=do, act 做，动      
+    agent 代理人  ag 做 ent 做什么的人；做事的人－－代理人；
+    agenda 议事日程；ag+enda 多数；做很多事情，要写清单－－议程；
+}
+
+agri(){
+agri, agro=agriculture 田地，农田 
+    agriculture 农业，农艺
+}
+
+ann(){
+  ann, enn=year 年    
+      annual 每年的，年度的
+}
+
+astro(){
+astro, aster=star 星  
+    astronomy 天文学
+}
+
+audi(){
+audi, audit=hear 听                          audible 听得见的
+}
+
+
+vint-vict{
+    //来源于拉丁语vincere战胜；Caesar平定叛乱后颁布三V文书；Veni Vedi Vici 拉丁语；意思是 I came, I saw, I conquered;到-看-胜；源于victoria 胜利女神；
+province; pro向前或出； vin胜利 ce名词后缀；古希腊把胜利后的地方划为省； 省；领域；职权；
+    provincial; 省的；地方性的；n. 乡下人；外地人；
+    provincially;以地方；偏狭地；
+    provinciality;乡下习气；地方特色；
+    provincialism; 地方主义； ism表...主义；
+victor 胜利者;
+    victory;胜利；
+    victorious;胜利的；
+    victoriously获胜地；
+victim; 受害者；-im表示不、无；相当于not;战斗中没胜利的--受害者；
+    tictimless; 无受害者； -less 名词后缀；
+    victimize;  vt.受害；
+    victimizer;    施害者；
+    victimization;  n.受害；
+vincible;
+convince;
+convict;罪犯；vt.定罪；宣判...有罪； n.罪犯； con-加强；共同；确保胜利--加强胜利成果--把xxx定罪；
+    conviction; 定罪；信念；确信；说明；-ion 名词后缀表性质、状态、程度；
+    convicted;定罪；宣判...有罪；
+    ex-convict; 从前曾被判刑过的人；前科犯；出狱者；犯过罪的人；
+    post-conviction;定罪后救济；post-表示...后；
+evict; 驱逐；e表示向外；-->向外扩张取得胜利->把人驱逐；
+    evictor;驱逐者；
+    eviction;n.驱逐；驱赶；
+    evicted;驱逐过去式；被驱逐的；
+    
+evince;
+
+}
