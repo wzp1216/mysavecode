d11ff312de4cd08381f59b89f59905c38e5ff1fa
wzp1216 <wzp1216@163.com>
2023-01-18 21:37:46 +0800

    20230118

--- english_PETS5/english_word.c
+++ english_PETS5/english_word.c
@@ -5,6 +5,7 @@ ann
 astro
 audi
 vint-vict
+form
 
 前缀prefix
 
@@ -61,13 +62,148 @@ convince;
 convict;罪犯；vt.定罪；宣判...有罪； n.罪犯； con-加强；共同；确保胜利--加强胜利成果--把xxx定罪；
     conviction; 定罪；信念；确信；说明；-ion 名词后缀表性质、状态、程度；
     convicted;定罪；宣判...有罪；
-    ex-convict; 从前曾被判刑过的人；前科犯；出狱者；犯过罪的人；
+    ex-convict; 从前曾被判刑过的人；前科犯；出狱者；犯过罪的人；ex-向外的；前任；ex-boyfriend;ex-president;ex-wife;
     post-conviction;定罪后救济；post-表示...后；
 evict; 驱逐；e表示向外；-->向外扩张取得胜利->把人驱逐；
     evictor;驱逐者；
     eviction;n.驱逐；驱赶；
     evicted;驱逐过去式；被驱逐的；
-    
-evince;
+evince; e-向外；  -e动词后缀； 向人展示胜利；--表明，显示；
+    vincible ible=able 能...的；可征服的，易克服的；
+    invincible 无敌的，不同征服的；in-否定前缀；
+    invincibility n.无敌；不可战胜；invincible +i +ty l e交换；e变为i;  -ibility ;i连接用； ty名词后缀；
+    n.无敌；不可
 
 }
+
+form(){
+    form  vi. 形成  n. 表格；
+    n.形成 形状 形态 外形 方式 表格 vt 构成 排列
+
+    formation; all right, formation;好了，组队；
+    formation ation复合名词后缀；阵形；编队 队形；
+    star-formation n.星形队形；
+    formative  -ate  -ive形容词后缀； adj 形成的；造型的；
+    formal  -al形容词后缀 正式的；
+    formally adv 正式地；
+    informal  adj. 非正式的； 
+    informally  adv. 非正式地；
+    formalize  formal+ -ize=ise 动词后缀；使形式化；使正式
+    formality  formal i -ty 名词后缀； 礼节；仪式；
+    life-form n.生物；
+    formalism  -ism名词后缀 抽象的含义,表...主义； 形式主义；
+    formalized formalize+ed 去一个e；adj形式化的；正式的；
+    waveform 波形；
+    malformation n.畸形 变形；  mal-坏
+    informality n. 非正式；不拘礼节； in-否定 + formality
+    formless  adj. 没有形状的；无定形的；形体不明的；
+    free-form  自由形态的；adj
+    unformed adj 未成形的；未充分发展的；
+    landform   地形； motherland  fatherland祖国 homeland security 国土安全局；landmark 地标；is-=dis-分；island 岛；mainland;大陆；inland内陆；
+    form-fitting adj 适体的，合身的；  fit 健康 ing 形容词后缀
+    malformed adj 畸形的，难看的；
+    forme 印版；  -e名词后缀；
+    well-formed  adj 身材苗条的，结构良好的；符合规则的；
+    formalization n 形式化；礼仪化；
+    formalist 形式主义者；
+    forming  n 形成；
+    semiformal  adj 半正式的  semi-  半；
+    formalistic adj  形式主义的；
+    habit-forming  adj 易上瘾的；
+    long-from 长格式； 
+    newlyformed  adj 成形的；
+    anti-reform 
+    multiform  adj 多样的，多种形式的；
+    informal-sector  非正式部门；  sector=sect 切 划分 部分--部门； -or名词后缀;
+    smog-forming adj 形成烟雾的；  smog-烟雾
+    cuneiform  adj 楔形的； n 楔形文字； cunei- 楔叶;
+    cruciform 十字形的； 十字架  cruc 十字，酷刑；
+    spongiform spong海绵；  adj 海绵状组织的；
+    half-formed adj 半成型的；
+    short-form 简易格式
+    piriform adj 梨形的；  pir=pear+ i +form  pear梨；
+    
+    inform 通知告诉；报告； in-使动(有时表否定) ；inform;使信息动到一起--收集信息--报告；
+    informed; 被通知的--消息多--消息灵通的；
+    uninformed;  adj 无知的；未被通知到的；
+    informant; -ant名词后缀，表人； 告密者；
+    informer; n. 告密者；通知者；
+    informate  vt inform + ate 动词后缀 ； 为...提供信息；告知，通知；
+    information; n 信息； ate+ion 名词后缀；
+    informative; adj 情报的；
+    informational; al形容词后缀 情报的；报告的；
+    bioinformatician;   bio=biology=bi+logy名词后缀 表...学；bio 生命，生物+informate+ic+i+an名词后缀表人  生物信息学者；
+    disinformation n 虚假信息； dis 分离；否定；
+    well-informed adj 消息灵通的；
+    misinform vt 误传 
+    misinformed vt 误传；
+    misinformation n 误报错误的消息；
+    informing ing 形容词后缀 表令...的；
+    //ing 表令...的；修饰主语为物； ed 表...的；修饰主语为人；interesting   interested exciting excited
+    information-based 信息化的，以信息为基础的；
+    information-gathering n 信息收集；
+    uninformative  adj 不提供信息的；不增长见闻的；
+    informatics n 情报学；信息学；ic+s复数  ics 学术名词后缀；
+    information-rich  adj 信息丰富的；
+    information-age n 信息时代；
+    bioinformatics  bio-informate+ics 生物信息学；
+    better-informed adj 消息灵通的；更明智的；
+    information-sharing n 信息共享；
+    informed-consent n 知情同意 con加强语气 sent=sens意义 consent同意；
+    best-informed 消息灵通的；
+    informativeness  信息量
+    information-processing n 信息处理；
+    //pro 向前 cess 走--进行，过程；  ing 动名词后缀；
+    information-technology 信息技术； techn 技术 -o -logy ...学;
+    ill-informed  adj 所知不多的；
+    
+
+    perform vi 表现； vt 预先形成 n 粗加工的成品;  per 完全 form ---完成形成 展现；
+    //perfect per 完全贯穿 fect 做  ---完全做完---完成，完美；
+    performance  表演  ance 名词后缀；
+    performative ate+ive 形容词或名词后缀；  adj表述行为的；n述行成分；
+    performer n 表演者
+    preformed  adj  预成型的； pre-在前 form形成；
+    outperform  out外面，超过；vt 胜过；做得比...好；
+    high-performance  adj 高性能的；
+    performance-enhancing  增强性能的；enhance提高； 
+    performance-based  绩效为基础的；
+    performance-orieted 绩效导向的； ori-升起---太阳升起---开始 初始+ent+ed   oriented 导向的；
+    //orient 使适应；确定方向；n 东方 adj 东方的
+    performance-related 性能相关的；
+    //re-反复lat拿 ed形容词后缀 related有关系的；
+
+
+    formula  n法则原则 数学公式； form ul a集合名词后缀；格式---公式---数学公式 ul=al 
+    formulate vt 规划  ate 动词后缀；
+    vormulation n 构想 规划 公式化  ion 名词后缀；
+    formlated 按配方制造的； ed形容词后缀；
+    formulaic   ic形容词后缀 公式化的，刻板的；套话堆砌的；
+    reformulate  re再 再用形式表示；
+    reformlated;
+    formulary  ary=ory 名词或形容词后缀；公式集；
+    formula-fed 配制饲料；
+    format  n （节目 计划等的）构成  v 使格式化； form形成 at=id 名词或动词后缀；
+    reformat  重定格式；
+    formatting  n 格式化；
+    large-format 大篇幅；
+    platform  平台 plat n 小地方 
+    cross-platform 跨平台；
+    transform；n.变形；trans- 改变； form 形式；tranforms变形金刚
+    transformation; n. 转型，变革； transform  ate  ion名词后缀；ate+ion=ation;  e不发音，则省略；
+    transformative; adj. 变化的，变形的；
+    transformer n. 变压器；促使变化的人；-er名词后缀；
+    transformational;     al形容词后缀；转变的，改变的，转换生成的；
+    transformed; ed;过去式；
+    log-transformed log--语言--符号--对数； 对数变换的；
+    untranformed; adj.未曾改变的；  un- 否定；ed形容词后缀；
+
+    former 从前的；前者的；前任的； n 模型 样板 起形成作用的人; form 形式--格式--模具+er名词后缀;
+    formerly  adv 以前；
+    uniform  adj 统一的；一致的；  n 制服 v 使穿制服  un-整体+i+form 双词根中间一般加i;----统一的形式；  unit=un 整体--单一 -it 名词或动词后缀；
+    uniformed  adj 穿着制服的；  ed形容词后缀;
+    uniformity i 连字符 -ty名词后缀；
+
+
+
+
---
+++ english_PETS5/essay.c
@@ -0,0 +1,11 @@
+
+
+
+Youth
+{
+    Youth is not a time of life; it is a state of mind;it is not a matter of rosy cheeks, red lips and supple nkees; it is a matter of the will, a quality of the imagination, a vigor of the emotions; it is the freshness of the deep springs of life.
+    Youth menas a temperamental predominanec of courage over timidity, of the appetite for adventure over the lov of ease.
+
+
+}
+
