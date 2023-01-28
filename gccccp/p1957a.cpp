cd3a6f48e862e32a12c3fefbc5eed6d7d35b2541
wzp1216 <wzp1216@gmail.com>
2023-01-28 15:51:44 +0800

    change p1957.cpp

--- gccccp/p1957a.cpp
+++ gccccp/p1957a.cpp
@@ -7,15 +7,13 @@ using namespace std;
 int main(){
 	int a,b,c, n;
 	cin>>n;
-	cin.get();
+	cin.ignore();
 	string st[n];
 	string yout[n];
+	char ch1,fuhao;
+	stringstream ss;
 	for (int i=0;i<n;i++){	
 		getline(std::cin,st[i]);
-	}
-	char ch1,fuhao;
-	stringstream ss,ss1;
-	for (int i=0;i<n;i++){	
 		ch1=st[i][0];
 		//cout<<ch1<<endl;
 		ss<<st[i];
@@ -23,19 +21,19 @@ int main(){
 			ss>>fuhao>>a>>b;
 		} else
 			ss>>a>>b;
-		if (fuhao=='a') {c=a+b;ss1<<a<<"+"<<b;}
-		else if (fuhao=='b') {c=a+b;ss1<<a<<"-"<<b;}
-		else if (fuhao=='c') {c=a+b;ss1<<a<<"*"<<b;}
-		ss1>>yout[i];
+        ss.clear();
+		if (fuhao=='a') {c=a+b;ss<<a<<"+"<<b;}
+		else if (fuhao=='b') {c=a-b;ss<<a<<"-"<<b;}
+		else if (fuhao=='c') {c=a*b;ss<<a<<"*"<<b;}
+		ss>>yout[i];
+        ss.clear();
 		ss<<yout[i]<<"="<<c;
 		ss>>yout[i];
-	}
-	
-	for (int i=0;i<n;i++){	
+        ss.clear();
 		cout<<yout[i]<<endl;
 		cout<<yout[i].size()<<endl;
 	}
-
+	
 	return 0;
 }
 
