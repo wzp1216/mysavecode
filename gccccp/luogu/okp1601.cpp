99ca735e5554e5f54ccf2ce31fe8b3bc397aa339
wzp1216 <wzp1216@163.com>
2023-02-09 21:18:06 +0800

    add today record

---
+++ gccccp/luogu/okp1601.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+int main(){
+    string astr,bstr;
+    cin>>astr>>bstr;
+    int a[510]={};int b[510]={},c[510]={};
+    int lena=astr.size(), lenb=bstr.size();
+    int lenc; lenc=lena>lenb?lena:lenb; lenc++;
+    for(int i=0;i<lena;i++) a[i]=astr[i]-'0';
+    for(int i=0;i<lenb;i++) b[i]=bstr[i]-'0';
+    for(int i=lenc;i>=0;i--){
+        if(i>=(lenc-lena)) a[i]=a[i+lena-lenc];
+        else a[i]=0;
+        if(i>=(lenc-lenb)) b[i]=b[i+lenb-lenc];
+        else b[i]=0;
+    }
+    int j=0;
+    for(int i=lenc;i>=0;i--){
+        int he=a[i]+b[i]+j;
+        c[i]=he%10;
+        j=he/10;
+    }
+    bool xian=0;
+    for(int i=0;i<lenc;i++){
+        if (c[i]!=0) xian=1;
+        if (xian) cout<<c[i];
+    }
+    if (xian==0) cout<<0;
+        return 0;
+}
