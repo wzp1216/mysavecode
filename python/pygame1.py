5f70980284d79e1f5eb5f4581081ba0dc83c9451
wzp1216 <wzp1216@163.com>
2023-05-26 09:39:14 +0800

    modified:   2023_no_video_do_youself.cpp

--- python/pygame1.py
+++ python/pygame1.py
@@ -4,6 +4,7 @@ screen=pygame.display.set_mode(size=(640,480))
 screen.fill(255,255,255)
 
 run=True
+j;alksjdf;lksjad;fkj
 while run:
     for event in pygame.get():
         if event.type==pygame.QUIT:
