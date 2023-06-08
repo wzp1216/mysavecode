6d6f39b278ccbce2b4339511b0b5eb8ee3c1199e
wzp1216 <wzp1216@163.com>
2023-06-08 18:43:01 +0800

    modified:   2023_no_video_do_youself.cpp 	modified:   mythink.cpp

--- opencv/test.py
+++ opencv/test.py
@@ -2,36 +2,37 @@ import sys
 import numpy as np
 import cv2 as cv
 
-
-
-def nothing(x):
+  
+def nothing():
     pass
 
-img=np.zeros((300,512,3),np.uint8)
-cv.namedWindow('img')
-
-
-cv.createTrackbar('R','img',0,255,nothing)
-cv.createTrackbar('G','img',0,255,nothing)
-cv.createTrackbar('B','img',0,255,nothing)
-
-switch='0: OFF \n1 :ON'
-cv.createTrackbar(switch,'img',0,1,nothing)
-
-while(1):
-    cv.imshow('img',img)
-    k=cv.waitKey(1) & 0xFF
-    if k==27: break
-
-    r=cv.getTrackbarPos('R','img')
-    g=cv.getTrackbarPos('G','img')
-    b=cv.getTrackbarPos('B','img')
-    s=cv.getTrackbarPos(switch,'img')
-
-    if s==0: img[:]=0
-    else:
-        img[:]=[b,g,r]
-
-
-
-
+  
+if __name__=='__main__':    
+   img=np.zeros((300,512,3),np.uint8)
+   cv.namedWindow('img')
+   
+   
+   cv.createTrackbar('R','img',0,255,nothing)
+   cv.createTrackbar('G','img',0,255,nothing)
+   cv.createTrackbar('B','img',0,255,nothing)
+   
+   switch='0: OFF \n1 :ON'
+   cv.createTrackbar(switch,'img',0,1,nothing)
+   
+   while(1):
+       cv.imshow('img',img)
+       k=cv.waitKey(2) & 0xFF
+       if k==27: break
+   
+       r=cv.getTrackbarPos('R','img')
+       g=cv.getTrackbarPos('G','img')
+       b=cv.getTrackbarPos('B','img')
+       s=cv.getTrackbarPos(switch,'img')
+   
+       if s==0: img[:]=0
+       else:
+           img[:]=[b,g,r]
+   
+   
+  
+  
