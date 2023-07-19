456b5199454f073b27ce2d6431113206f7c07959
wzp1216@163.com <wzp1216@163.com>
2023-07-19 22:20:54 +0800

    changed files on 2023-07-19 22:20:1689776454

--- pushgit.sh
+++ pushgit.sh
@@ -4,11 +4,11 @@ echo git-pull-add-push:$CURRENT_DIR
 echo;
  
 echo 本地主分支拉取远程主分支：git pull 
-git pull 
+git pull  -y
 echo;
 
 echo 开始添加变更：git add .
-git add .
+git add . -y
 echo;
  
 set 提交的commit信息为时间；
@@ -17,7 +17,7 @@ git commit -m "changed files on `date +'%Y-%m-%d %H:%M:%s'`";
 echo;
  
 echo 将变更情况提交到远程自己分支：git push 
-git push 
+git push -y
 echo;
  
 
