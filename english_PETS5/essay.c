71c1059803e535dcf0f552e622be4e3d4c00caaf
wzp1216@163.com <wzp1216@163.com>
2023-07-28 22:20:12 +0800

    changed files on 2023-07-28 22:20:1690554012

--- english_PETS5/essay.c
+++ english_PETS5/essay.c
@@ -11,3 +11,11 @@ Youth()
     when your aerials are down, and your spirit is covered with snows of cynicism and the ice of pessimism, then you've grown old, even at 20; but as long as your aerials are up, to catch waves of optimism, there's hope you may die young at 80.
 }
 
+Three_Days_to_See()
+{
+    All of us have read thrilling stories in which the hero had only a limited and specified time to live. Sometimes it was as long as a year, sometimes as short as 24 hours. But always we were interested im discovering just how the doomed hero chose to spend his last days or his last hours. I speak, of course of free men who have a choice, not condemend criminals whose sphere of activities is strictly delimited.
+    Such stories set us thinking, wondering what we should do under similar circumstances. What events, what experiencds, what associations shouldwe crowd into those last hours as mortal beings,what regrets?
+    Sometimes Ihave thought it would be an excellent rule to live each day as if we sould die tomorrow. Such an attitude would emphasize sharply the values of life.We should live each day with gentleness, vigor and a keenness of appreciation which are often lost when time stretches befor us in the constant panorama of more days and months and years to come. There are those, of course, who would adopt the Epicurean motto of "Eat, drink and be merry". But most people would be chastened by the certainty of impending death.
+    In sotries the dommed hero is usually saved at the last minute by some stroke of fortune, but almost always his sene of values is changed. He becomes mor appreciative of the meaning of life and its permanent spiritual values. It has often been noted that those who live or have lived, in the shadow of death bring a mellow sweetness to everything they do.
+
+}
