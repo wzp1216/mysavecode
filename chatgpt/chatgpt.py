89e5f2fbcb3491d4832db29486b01438d5725205
wzp1216 <wzp1216@163.com>
2023-08-07 17:38:02 +0800

    changed files on 2023-08-07 17:38:1691401082

--- chatgpt/chatgpt.py
+++ chatgpt/chatgpt.py
@@ -6,17 +6,6 @@ import json
 
 
 
-completion=openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role":"user","content":"can you tell me a english story?"},
-        {"role":"user","content":"give me a next english story"}
-
-
-    ]
-    
-)
-
+completion=openai.ChatCompletion.create(model="gpt-3.5",messages=[{"role":"user","content":"can you tell me a english story?"},{"role":"user","content":"give me a next english story"}])
 print(completion)
-
 
