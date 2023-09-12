19ec90ffa99f32d9be5f84dae359cc9929c4fe21
wzp1216 <wzp1216@163.com>
2023-09-12 23:01:26 +0800

    chang main_dlg

--- AAxinopencv/main_dlg.py
+++ AAxinopencv/main_dlg.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDesktopWidget
 from PyQt5.QtWidgets import QCheckBox,QLabel,QGraphicsView,QGraphicsScene,QGraphicsPixmapItem
 from PyQt5.QtWidgets import QVBoxLayout,QGridLayout
@@ -20,24 +20,26 @@ class main_dlg(QWidget):
     def init(self):
         lay=QGridLayout()
         lay.setSpacing(10)
-        noImg=QImage("./gui/no_img.png")
+        img1=QImage("./gui/no_img.png")
         imgLab1=QLabel()
-        imgLab1.setPixmap(QPixmap.fromImage(noImg))
-        lay.addWidget(imgLab1,0,0,4,1)
+        imgLab1.setFixedSize(400,400)
+        img1=img1.scaled(400,400,Qt.IgnoreAspectRatio,Qt.SmoothTransformation)
+        imgLab1.setPixmap(QPixmap.fromImage(img1))
+        lay.addWidget(imgLab1,0,0,1,1)
         imgLab2=QLabel()
-        imgLab2.setPixmap(QPixmap.fromImage(noImg))
-        lay.addWidget(imgLab2,0,1,4,1)
+        imgLab2.setPixmap(QPixmap.fromImage(img1))
+        lay.addWidget(imgLab2,0,1,1,1)
         imgLab3=QLabel()
-        imgLab3.setPixmap(QPixmap.fromImage(noImg))
-        lay.addWidget(imgLab3,4,0,4,1)
+        imgLab3.setPixmap(QPixmap.fromImage(img1))
+        lay.addWidget(imgLab3,1,0,1,1)
         imgLab4=QLabel()
-        imgLab4.setPixmap(QPixmap.fromImage(noImg))
-        lay.addWidget(imgLab4,4,1,4,1)
+        imgLab4.setPixmap(QPixmap.fromImage(img1))
+        lay.addWidget(imgLab4,1,1,1,1)
         imgLab_top=QLabel()
-        imgLab_top.setPixmap(QPixmap.fromImage(noImg))
-        lay.addWidget(imgLab_top,0,2,4,1)
+        imgLab_top.setPixmap(QPixmap.fromImage(img1))
+        lay.addWidget(imgLab_top,0,2,1,1)
         lab_msg=QLabel("system is start...")
-        lay.addWidget(lab_msg,8,0,1,3)
+        lay.addWidget(lab_msg,2,0,1,3)
         self.setLayout(lay)
 
 
